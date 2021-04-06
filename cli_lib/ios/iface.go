2ba14c8310aa4a07bb5737ea32b5cdf07f183fec
hopwesley <ws@sinoc.io>
2021-04-06 20:03:34 +0800

    ios build

--- cli_lib/ios/iface.go
+++ cli_lib/ios/iface.go
@@ -1,23 +1,46 @@
 package iosLib
 
-import "github.com/ninjahome/ninja-go/wallet"
+import (
+	"github.com/ninjahome/ninja-go/wallet"
+)
 
 func NewWallet(auth string) string {
 	key := wallet.NewKey()
+	_inst.key = key
 	return key.StoreString(auth)
+}
+
+func ActiveAddress() string {
+	if _inst.key == nil {
+		return ""
+	}
+
+	return _inst.key.Address.String()
 }
 
 type IosApp struct {
 	key *wallet.Key
+	cb  AppCallBack
 }
 
 var _inst = &IosApp{}
 
-func InitApp(cipherTxt, auth string) error {
+type AppCallBack interface {
+	//ImmediateMessage(from, to string, payload []byte, time int64) error
+	//WebSocketClosed()
+	//UnreadMsg(jsonData []string) error
+}
+
+func InitApp(cipherTxt, auth string, callback AppCallBack) error {
 	parsedKey, err := wallet.LoadKeyFromJsonStr(cipherTxt, auth)
 	if err != nil {
 		return err
 	}
 	_inst.key = parsedKey
+	_inst.cb = callback
 	return nil
+}
+
+func WriteMessage(To string, payload []byte) {
+
 }
