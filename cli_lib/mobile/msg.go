30932371335ed8c552d1ce6225736d688a30e425
megaliao <micliao@tutanota.com>
2021-09-13 09:09:15 +0800

    add to repo

--- cli_lib/mobile/msg.go
+++ cli_lib/mobile/msg.go
@@ -11,3 +11,12 @@ func RecoverGroupMsg(msgCount int, groupId string, time int64) {
 func RecoverMsg(msgCount int, to string, time int64) {
 
 }
+
+//if startTime == endTime, just delete one msg
+//if startTime == endTime == 0, delete all chat msg
+//if startTime < endTime, error
+//id means userId or groupId
+//if id == "", clear all msg between startTime and endTime
+func ClearMsg(startTime, endTime int64, id string) error {
+	return nil
+}
